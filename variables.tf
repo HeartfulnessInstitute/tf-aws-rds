b60188b72fecb5ce076db58725cea6431ad5289c
vijaylaxmi.radder <vijaylaxmi.radder@volunteer.heartfulness.org>
2025-08-01 10:48:39 +0530

    added new files

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "allowed_cidr_blocks" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
