cb52d00b63ae4229d36509f70e9b8b2371f7f644
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-15 15:07:50 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,30 @@
+output "db_instance_id" {
+  description = "RDS instance identifier (id)"
+  value       = aws_db_instance.this.id
+}
+
+output "db_instance_arn" {
+  description = "RDS instance ARN"
+  value       = aws_db_instance.this.arn
+}
+
+output "endpoint" {
+  description = "RDS endpoint address"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "address" {
+  description = "Connection address"
+  value       = aws_db_instance.this.address
+}
+
+output "port" {
+  description = "RDS port"
+  value       = aws_db_instance.this.port
+}
+
+output "subnet_group_name" {
+  description = "DB subnet group name"
+  value       = aws_db_subnet_group.this.name
+}
 
