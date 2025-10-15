5b049adee3cc6baa3651c1fbe918066f03250fba
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-15 13:38:02 +0530

    Update output.tf

--- modules/rds-mysql/output.tf
+++ modules/rds-mysql/output.tf
@@ -1,13 +1,19 @@
-# output "rds_endpoint" {
-#   description = "RDS instance endpoint"
-#   value       = aws_db_instance.mysql.endpoint
-# }
+output "db_instance_id" {
+  value = aws_db_instance.this.id
+}
 
-# output "rds_sg_id" {
-#   description = "RDS security group ID"
-#   value       = aws_security_group.rds_sg.id
-# }
+output "db_instance_endpoint" {
+  value = aws_db_instance.this.endpoint
+}
 
-# output "db_secret_arn" {
-#   value = aws_secretsmanager_secret.db_secret.arn
-# }
+output "db_instance_address" {
+  value = aws_db_instance.this.address
+}
+
+output "db_subnet_group_name" {
+  value = local.subnet_group_name
+}
+
+output "db_security_group_id" {
+  value = aws_security_group.this.id
+}
