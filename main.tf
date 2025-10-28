946cdf716a7f8434d9e52e8531eaa2d40b57a761
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-28 13:13:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,11 +25,7 @@ data "aws_vpc" "existing" {
 data "aws_subnets" "existing" {
   count = var.use_existing_vpc && var.auto_discover_subnets ? 1 : 0
 }
-  
-  filter {
-    name   = "vpc-id"
-    values = [var.vpc_id]
-  }
+
 ############################################
 # DB Subnet Group
 ############################################
