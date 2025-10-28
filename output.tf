1661554bfb0597456445fe7e8a57fe58d33c6568
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-28 13:24:40 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,45 @@
-output "rds_endpoint" {
-  description = "RDS instance endpoint"
-  value       = aws_db_instance.this.endpoint
+output "db_instance_id" {
+  description = "The RDS instance ID"
+  value       = aws_db_instance.this.id
 }
 
-output "rds_arn" {
-  description = "RDS instance ARN"
+output "db_instance_arn" {
+  description = "The ARN of the RDS instance"
   value       = aws_db_instance.this.arn
 }
 
-output "subnet_group_name" {
-  description = "DB subnet group name"
+output "db_instance_endpoint" {
+  description = "The connection endpoint"
+  value       = aws_db_instance.this.endpoint
+}
+
+output "db_instance_address" {
+  description = "The hostname of the RDS instance"
+  value       = aws_db_instance.this.address
+}
+
+output "db_instance_port" {
+  description = "The database port"
+  value       = aws_db_instance.this.port
+}
+
+output "db_instance_name" {
+  description = "The database name"
+  value       = aws_db_instance.this.db_name
+}
+
+output "db_instance_username" {
+  description = "The master username for the database"
+  value       = aws_db_instance.this.username
+  sensitive   = true
+}
+
+output "db_security_group_id" {
+  description = "The security group ID of the RDS instance"
+  value       = aws_security_group.this.id
+}
+
+output "db_subnet_group_name" {
+  description = "The db subnet group name"
   value       = aws_db_subnet_group.this.name
 }
