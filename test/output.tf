354226bd64257a18ec1bc3f1cf482cc42a26f0b4
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-28 11:04:05 +0530

    Delete test directory

--- test/output.tf
+++
@@ -1,9 +0,0 @@
-output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
-  value       = module.rds.rds_endpoint
-}
-
-output "rds_security_group_id" {
-  description = "Security Group ID for RDS access"
-  value       = module.rds.rds_sg_id
-}
