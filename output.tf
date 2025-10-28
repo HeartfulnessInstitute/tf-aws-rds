b3cdcdb19e06d750457ae41384915fe10e554cff
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-28 20:39:05 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,24 +1,11 @@
-output "db_instance_id" {
-  description = "RDS instance ID"
-  value       = aws_db_instance.this.id
-}
-
 output "db_endpoint" {
-  description = "RDS endpoint"
-  value       = aws_db_instance.this.endpoint
+  value = aws_db_instance.main.endpoint
 }
 
-output "db_security_group_id" {
-  description = "DB security group id created or provided (first found)"
-  value = (
-    length(aws_security_group.default) > 0 ? aws_security_group.default[0].id :
-    (length(var.security_group_ids) > 0 ? var.security_group_ids[0] :
-      (length(var.vpc_security_group_ids) > 0 ? var.vpc_security_group_ids[0] : null))
-  )
+output "db_port" {
+  value = aws_db_instance.main.port
 }
 
-
-output "db_subnet_group_name" {
-  description = "DB subnet group name"
-  value       = aws_db_subnet_group.this.name
+output "db_name" {
+  value = aws_db_instance.main.db_name
 }
